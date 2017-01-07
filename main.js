0168931d486b88d7a39dcc18ef4c872653b9beed
Radu Popescu <radumihailpopescu@gmail.com>
2017-01-07 16:31:05 +0200

    Added node-grovepi

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.print("asd");
+var GrovePi = require('node-grovepi').GrovePi
+
+// var Commands = GrovePi.commands
+// var Board = GrovePi.board
+
+var board = new Board({
+    debug: true,
+    onError: function(err) {
+      console.log('Something wrong just happened')
+      console.log(err)
+    },
+    onInit: function(res) {
+      if (res) {
+        console.log('GrovePi Version :: ' + board.version())
+
+        var lightSensor = new LightAnalogSensor(0)
+        console.log('Light Analog Sensor (start watch)')
+        lightSensor.on('change', function(res) {
+          console.log('Light onChange value=' + res)
+        })
+        lightSensor.watch()
+      }
+    }
+  })
+
+console.log("started");
